64ffa30f339dffed6135e6d4bb439309755fd706
Bowtied HarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2023-08-27 04:11:17 -0500

    complete lesson 7 part 4 debugging tests 1

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -14,4 +14,8 @@ contract FundMeTest is Test {
     function testMinimumDollarIsFive() public {
         assertEq(fundMe.MINIMUM_USD(), 5e18);
     }
+
+    function testOwnerIsMessageSender() public {
+        assertEq(fundMe.i_owner(), address(this));
+    }
 }
