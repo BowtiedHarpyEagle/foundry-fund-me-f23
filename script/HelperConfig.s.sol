8f162c57610e3f34a93a62823baee05450016663
Bowtied HarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2023-08-29 09:06:49 -0500

    add mainnet eth and of lesson 7 part 9

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -13,6 +13,8 @@ contract HelperConfig {
     constructor() {
         if (block.chainid == 11155111) {
             activeNetworkConfig = getSepoliaEthConfig();
+        } else if (block.chainid == 1) {
+            activeNetworkConfig = getMainnetEthConfig();
         } else {
             activeNetworkConfig = getAnvilEthConfig();
         }
@@ -24,6 +26,14 @@ contract HelperConfig {
         });
 
         return sepoliaConfig;
+    }
+
+    function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory ethConfig = NetworkConfig({
+            priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        });
+
+        return ethConfig;
     }
 
     function getAnvilEthConfig() public pure returns (NetworkConfig memory) {}
