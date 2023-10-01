68dffd2b83266e675025720415bdf23def715ddd
BowtiedHarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2023-10-01 14:14:24 -0500

    get mostRecentlyDeployed address L7P19 6:06

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -6,10 +6,12 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
-
+import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
 contract FundFundMe is Script {
     function run() external {
-        
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
+
+
     }
 }
 
