d1a2cf2d4f5aae11838a08289b63b07bee94525b
BowtiedHarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2023-10-01 14:30:12 -0500

    complete L7P19 08:06 function fundFundMe

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -5,12 +5,22 @@
 
 pragma solidity ^0.8.18;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
+import {FundMe} from "../src/FundMe.sol";
+
 contract FundFundMe is Script {
+    uint256 constant SEND_VALUE = 0.01 ether; 
+    function fundFundme (address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed).fund{value: SEND_VALUE}();
+        vm.stopBroadcast();
+        console.log("FundMe contract funded with %s", SEND_VALUE);
+    }
+
     function run() external {
         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment("FundMe", block.chainid);
-
+        
 
     }
 }
