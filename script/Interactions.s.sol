7a664a7f285471c069da8ab14caa8632827ba3d3
BowtiedHarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2023-10-01 13:49:45 -0500

    complete L7P19 3:16

---
+++ script/Interactions.s.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: MIT
+
+//Fund 
+//Withdraw
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+
+contract FundFundMe is Script {
+    function run() external {
+        
+    }
+}
+
+contract WithdrawFundME is Script {
+
+}
