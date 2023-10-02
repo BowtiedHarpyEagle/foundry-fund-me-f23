e950f25278b733bfe73b5d664736685d5fe9d50a
BowtiedHarpyEagle <140409172+BowtiedHarpyEagle@users.noreply.github.com>
2023-10-02 09:11:22 -0500

    add vm.startBroadcast and stopbroadcast

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -32,7 +32,9 @@ contract FundFundMe is Script {
 
 contract WithdrawFundMe is Script {
         function withdrawFundMe (address mostRecentlyDeployed) public {
+        vm.startBroadcast();
         FundMe(payable(mostRecentlyDeployed)).withdraw();
+        vm.stopBroadcast();
     }
 
     function run() external {
